6c11ca50b0732d4543f02f64ca2fc7202b81edfb
Anurag <kr.anurag24@gmail.com>
2023-01-18 11:59:47 +0530

    add progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import "@fontsource/pt-mono";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     // <CandyPayProvider publicApiKey={process.env.NEXT_PUBLIC_CP_API}>
     <QueryClientProvider client={queryClient}>
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
       <Component {...pageProps} />
     </QueryClientProvider>
     // </CandyPayProvider>
