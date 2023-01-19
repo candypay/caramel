f54046ddc6c5193669ff39c0cf36a079aeeaf89f
Anurag <kr.anurag24@gmail.com>
2023-01-19 19:43:23 +0530

    err

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -2,6 +2,7 @@ import { InstallElement } from "@/components/Elements/Install";
 import { NPMElement } from "@/components/Elements/NPM";
 import Select from "@/components/Elements/Select";
 import Header from "@/components/Misc/Header";
+import { ErrorComponent } from "@/components/Sections/Error";
 import { withSuccess } from "@/middlewares/withPackage";
 import { installationTool } from "@/typings/installationTool";
 import { ISuccessPageProps } from "@/typings/props";
@@ -16,6 +17,7 @@ const Paid: NextPage<ISuccessPageProps> = ({
   installation_url,
 }) => {
   const [tool, setTool] = useState<installationTool>("pnpm");
+  console.log(isExpired, isInvalid);
 
   return (
     <div className="h-screen w-full flex bg-[#0F111E] text-white">
@@ -26,21 +28,28 @@ const Paid: NextPage<ISuccessPageProps> = ({
           backgroundImage: "url('/assets/bg-pattern.svg')",
         }}
       >
-        <Image src="/assets/check.svg" width={50} height={50} alt="check" />
+        {isExpired && <ErrorComponent type="expired" />}
+        {isInvalid && <ErrorComponent type="invalid" />}
+        {!isExpired && !isInvalid && (
+          <>
+            <Image src="/assets/check.svg" width={50} height={50} alt="check" />
 
-        <p className="font-secondary text-2xl">Successfully Purchased</p>
-        <p className="font-primary">
-          First, add this to your{" "}
-          <span className="text-purple-300">.npmrc</span> file:
-        </p>
+            <p className="font-secondary text-2xl">Successfully Purchased</p>
+            <p className="font-primary">
+              First, add this to your{" "}
+              <span className="text-purple-300">.npmrc</span> file:
+            </p>
 
-        <NPMElement {...{ token }} />
+            <NPMElement {...{ token }} />
 
-        <p>
-          To install the package, run the following command in your terminal:
-        </p>
-        <InstallElement {...{ installation_url, tool }} />
-        <Select {...{ tool, setTool }} />
+            <p>
+              To install the package, run the following command in your
+              terminal:
+            </p>
+            <InstallElement {...{ installation_url, tool }} />
+            <Select {...{ tool, setTool }} />
+          </>
+        )}
       </div>{" "}
     </div>
   );
